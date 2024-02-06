646844ede74d51e9cc711dc334f67691305f9de2
deborah cherotich <cherotichdeborahb@gmail.com>
2024-02-06 22:56:01 +0300

    add kotlin

---
+++ Main.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var fullName = "Deborah Cherotich"
+    println(fullName)
+
+    var age = 19
+    println(age)
+
+    var phoneNumber = 700870092
+    println(phoneNumber)
+
+    var weight = 54.7
+    println(weight)
+
+    var citizen = "Kenyan"
+    println(citizen)
+
+}
